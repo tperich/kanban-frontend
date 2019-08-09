bc05cdce6311b8bbb1b303353e5f1182e2488e4b
Tomislav Peric <toca91@gmail.com>
2019-08-09 13:45:54 +0200

    Add antd; improve styles

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'antd';
 import { Droppable } from 'react-beautiful-dnd';
 
 import './Column.scss';
 import Task from '../Task/Task';
+import '../../styles/buttons.scss';
+
+const addTask = columnId => {
+  console.log('Adding task to ', columnId);
+};
 
 function Column({ columnId, title = 'To do', tasks }) {
   return (
@@ -25,13 +31,19 @@ function Column({ columnId, title = 'To do', tasks }) {
           </div>
         )}
       </Droppable>
+      <Button
+        className="add-btn--centered"
+        shape="circle"
+        icon="plus"
+        onClick={() => addTask(columnId)}
+      />
     </div>
   );
 }
 
 Column.propTypes = {
   title: PropTypes.string,
-  tasks: PropTypes.array
+  tasks: PropTypes.array,
 };
 
 export default Column;
--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Button } from 'antd';
 
 import { fetchBoard } from '../../store/actions/actions';
 import Board from '../../components/Board/Board';
 import './Home.scss';
-import '../../styles/textStyles.scss';
+import '../../styles/text.scss';
 
 function Home() {
   const dispatch = useDispatch();
@@ -19,7 +20,7 @@ function Home() {
     if (!boardsData.isFetching && !boardsData.error) {
       return <Board data={boardsData.board} />;
     } else if (boardsData.isFetching && !boardsData.error) {
-      return <h1>Loading...</h1>;
+      return <Button type="dashed" shape="circle-outline" loading />;
     } else if (boardsData.error) {
       return (
         <h1 className="message--error">
